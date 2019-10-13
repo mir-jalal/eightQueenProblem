0657654713c55a32cfbfac88e9bf82b7a8426ec6
Mir Jalal <35169094+mircelal99@users.noreply.github.com>
2019-10-14 02:25:20 +0400

    Add files via upload

--- eight_queen_problem.py
+++ eight_queen_problem.py
@@ -1,10 +1,12 @@
 import chess_board
 import tkinter
 
+
 def start_program():
-    n=int(text_box.get())
+    n = int(text_box.get())
     root.destroy()
     chess_board.chess_board(n)
+
 
 root = tkinter.Tk()
 root.title("N queen Problem")
@@ -12,11 +14,11 @@ root.minsize(width=500, height=250)
 root.maxsize(width=500, height=250)
 
 label = tkinter.Label(root, text="Board Size: ")
-label.place(x = 40, y = 100)
-label.config(font=("Calibri",20))
+label.place(x=40, y=100)
+label.config(font=("Calibri", 20))
 
 text_box = tkinter.Entry(root, width=8)
-text_box.place(x = 200, y=100)
+text_box.place(x=200, y=100)
 text_box.config(font=("Calibri", 20))
 
 button = tkinter.Button(root, text="Start", command=start_program)
--- eight_queen_solution.py
+++ eight_queen_solution.py
@@ -3,47 +3,52 @@ def unsafe_queens(arr):
 
     for i in range(len(arr)):
         for j in range(len(arr[i])):
-            if(arr[i][j]==1 and is_unsafe(arr,i,j)):
-                unsafe_queens_arr.append([i,j,True])
-            elif(arr[i][j]==1):
-                unsafe_queens_arr.append([i,j,False])
+            if(arr[i][j] == 1 and is_unsafe(arr, i, j)):
+                unsafe_queens_arr.append([i, j, True])
+            elif(arr[i][j] == 1):
+                unsafe_queens_arr.append([i, j, False])
     return unsafe_queens_arr
+
 
 def is_unsafe(arr, x, y):
 
     for i in range(len(arr)):
-        if(i!=y and arr[x][i]==1):
+        if(i != y and arr[x][i] == 1):
             return True
-        if(i!=x and arr[i][y]==1):
+        if(i != x and arr[i][y] == 1):
             return True
 
-    for i in range(1,len(arr)):
-        if(x+i<len(arr) and y+i<len(arr) and arr[x+i][y+i]==1):
+    for i in range(1, len(arr)):
+        if(x + i < len(arr) and y + i < len(arr) and arr[x + i][y + i] == 1):
             return True
-        if(x-i>-1 and y-i>-1 and arr[x-i][y-i]==1):
+        if(x - i > -1 and y - i > -1 and arr[x - i][y - i] == 1):
             return True
-        if(x+i<len(arr) and y-i>-1 and arr[x+i][y-i]==1):
+        if(x + i < len(arr) and y - i > -1 and arr[x + i][y - i] == 1):
             return True
-        if(x-i>-1 and y+i<len(arr) and arr[x-i][y+i]==1):
+        if(x - i > -1 and y + i < len(arr) and arr[x - i][y + i] == 1):
             return True
+
+
 def all_safe_queens(arr):
     for i in range(len(arr)):
         for j in range(len(arr[i])):
-            if(arr[i][j]==1 and is_unsafe(arr,i,j)):
+            if(arr[i][j] == 1 and is_unsafe(arr, i, j)):
                 return False
-
     return True
+
 
 def is_safe(i, j, right_diagonal_arr, left_diagonal_arr, right_diagonal_arr_look, left_diagonal_arr_look, row_arr):
     if right_diagonal_arr_look[right_diagonal_arr[i][j]] or left_diagonal_arr_look[left_diagonal_arr[i][j]] or row_arr[i]:
         return False
     return True
 
+
 def column_occupied(arr, j):
     for i in range(j):
-        if arr[i][j]==1:
+        if arr[i][j] == 1:
             return True
     return False
+
 
 def queen_counter(arr):
     counter = 0
